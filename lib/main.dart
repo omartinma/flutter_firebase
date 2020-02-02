b1c9503d48741c73b446fe414cb9060ce6d88259
Oscar Martin <martinm.oscar@gmail.com>
2020-02-02 12:15:48 +0100

    Adding login page without functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/screens/home/home_page.dart';
+import 'package:flutter_firebase/screens/login/login_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,36 +12,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Welcome to Flutter',
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/screens/login/login_page.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key}) : super(key: key);
+
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            color: Colors.white,
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text("Let´s stop mess up with infinty to-do text list", textAlign: TextAlign.center,style: TextStyle(fontSize: 30, ),),
+                // Form: user y pwd
+                Container(
+                  margin:
+                      EdgeInsets.only(left: 40, right: 40, top: 30, bottom: 20),
+                  child: new Column(
+                    children: <Widget>[
+                      TextField(
+                          autocorrect: false,
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                              labelText: "Usuario",
+                              icon: Icon(Icons.account_circle))),
+                      TextField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            labelText: "Contraseña", icon: Icon(Icons.lock)),
+                      )
+                    ],
+                  ),
+                ),
+                // Login button
+                Container(
+                    padding: EdgeInsets.all(30),
+                    width: MediaQuery.of(context).size.width,
+                    child: FlatButton(
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      disabledColor: Colors.grey,
+                      disabledTextColor: Colors.black,
+                      padding: EdgeInsets.all(20.0),
+                      splashColor: Colors.blue,
+                      onPressed: () {
+                        _signInWithEmailAndPassword();
+                      },
+                      child: Text(
+                        "Login",
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                              color: Colors.transparent,
+                              width: 1,
+                              style: BorderStyle.solid),
+                          borderRadius: BorderRadius.circular(50)),
+                    )),
+              ],
+            )));
+  }
+
+  void _signInWithEmailAndPassword() async {
+    /*final FirebaseAuth _auth = FirebaseAuth.instance;
+    FirebaseUser user;
+    try {
+      user = (await _auth.signInWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      ))
+          .user;
+    } catch (e) {}
+
+    if (user != null) {
+      setState(() {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      });
+    } else {
+      showAlertDialog(context);
+    }
+    */
+  }
+
+  showAlertDialog(BuildContext context) {
+
+  // set up the button
+  Widget okButton = FlatButton(
+    child: Text("OK"),
+    onPressed: () { 
+      Navigator.of(context).pop(); // dismiss dialog
+    },
+  );
+
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("Error login"),
+    content: Text("Comprueba que has introducido bien email y contraseña"),
+    actions: [
+      okButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
+}

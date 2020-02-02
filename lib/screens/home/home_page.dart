48e5c234a8556ecf8707070fa782f84e76ec0990
Oscar Martin <martinm.oscar@gmail.com>
2020-02-02 16:01:19 +0100

    Button to home page

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -13,37 +13,58 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter Demo Home Page"),
-        automaticallyImplyLeading: false,
-        actions: <Widget>[
-          // overflow menu
-          PopupMenuButton(
-            itemBuilder: (context) => [
-              PopupMenuItem(
-                value: 1,
-                child: Text("Logout"),
-              ),
-            ],
-            onSelected: (value) async {
-              bool result = await UserController().signOut();
-              if (result) {
-                   Navigator.of(context).pushReplacementNamed('/login');
-              }
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Welcome to Flutter',
+        appBar: AppBar(
+          title: Text("Flutter Demo Home Page"),
+          automaticallyImplyLeading: false,
+          actions: <Widget>[
+            // overflow menu
+            PopupMenuButton(
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  value: 1,
+                  child: Text("Logout"),
+                ),
+              ],
+              onSelected: (value) async {
+                bool result = await UserController().signOut();
+                if (result) {
+                  Navigator.of(context).pushReplacementNamed('/login');
+                }
+              },
             ),
           ],
         ),
-      ),
-    );
+        body: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                    padding: EdgeInsets.all(30),
+                    width: MediaQuery.of(context).size.width,
+                    child: FlatButton(
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      disabledColor: Colors.grey,
+                      disabledTextColor: Colors.black,
+                      padding: EdgeInsets.all(20.0),
+                      splashColor: Colors.blue,
+                      onPressed: () {
+                      },
+                      child: Text(
+                        "TV SHOWS",
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                              color: Colors.transparent,
+                              width: 1,
+                              style: BorderStyle.solid),
+                          borderRadius: BorderRadius.circular(50)),
+                    )),
+              ],
+            ),
+          ),
+        ));
   }
 }
